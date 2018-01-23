3e850b3a9dc9e5f28b12d8fe02a8c01fdc9d43a9
Sam Lea <samuel.lea@live.co.uk>
2018-01-23 15:22:44 +0000

    Implemented functionality for fetching user's profile data, for use in seeding DB

--- controllers/spotifyAuth.js
+++ controllers/spotifyAuth.js
@@ -1,4 +1,5 @@
-const {authApp, getToken, getTopArtistsAndTracks} = require('../models/spotify');
+const {authApp, getToken, getTopArtistsAndTracks, fetchSpotifyProfile} = require('../models/spotify');
+const {formatTrack, formatArtists, formatGenres} = require('../models/formatting');
 
 function authorise(req, res, next) {
 	authApp(req, res, next);
@@ -7,8 +8,21 @@ function authorise(req, res, next) {
 function sendProfileData(req, res, next) {
 	getToken(req, res, next)
 		.then(tokens => getTopArtistsAndTracks(tokens, res))
-		.then(data => res.send(data));
+		.then(([tokens, data]) => {
+
+
+			data.topTracks = formatTrack(data.topTracks);
+			data.genres = formatGenres(data.topArtists);
+			data.topArtists = formatArtists(data.topArtists);
+
+			// res.send([formattedTrack, formattedArtists, formattedGenres]);
+			return Promise.all([data, fetchSpotifyProfile(tokens)]);
+      
+		})
+		.then(([data, [tokens, body]]) => res.send([data, [tokens, body]]));
 }
+
+
 
 
 module.exports = {authorise, sendProfileData};
